91ae46800a7af1b6d997da9cda7fbe86f430f5c2
Tony <anthonychung14@gmail.com>
2020-03-20 08:13:48 -0700

    Single author ref

--- app/models/source_content.rb
+++ app/models/source_content.rb
@@ -1,6 +1,7 @@
 class SourceContent < ApplicationRecord
   # has_many :excerpts
   # has_many :source_card
+  belongs_to :author
   has_many :authors_source_content_works
   has_many :authors, through: :source_content_works
   has_many :excerpts
