aa6f8620740925c80e151cc63d72ea407b9fdee6
Tony <anthonychung14@gmail.com>
2020-03-20 20:22:22 -0700

    Add category

--- app/graphql/types/content_category.rb
+++ app/graphql/types/content_category.rb
@@ -1,5 +1,6 @@
 class Types::ContentCategory < Types::BaseEnum
   value "PODCAST", "A podcast"
+  value "VIDEO", "A generic video"
   value "IMAGE", "A still image, such as a photo or graphic"
   value "BLOG_POST", "A self-published, short-form article"
   value "ARTICLE", "A peer-reviewed article published through an academic institution"
